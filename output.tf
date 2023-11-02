5f57ab2f895a3fa66d4fe22b4e0ce1237c34d6d9
Jo Schwandke <59925842+j0g3sc@users.noreply.github.com>
2023-11-02 11:47:47 +0100

    Add files via upload

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "count" {
+  type = number
+  default = val.count
+}
+
+output "text" {
+  type = string
+  default = "val.text
+}
+
+output "flag" {
+  type = bool
+  default = val.flag
+}
---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "count" {
+  type = number
+  default = 0
+}
+
+variable "text" {
+  type = string
+  default = "foo"
+}
+
+variable "flag" {
+  type = bool
+  default = false
+}
