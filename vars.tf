77e1a79f07b4213b1f023df6373d0b583a37d124
Thomas Felix <tfelix@meshcloud.io>
2024-01-12 14:48:27 +0100

    fixes second bb

--- vars.tf
+++ vars.tf
@@ -9,6 +9,18 @@ variable "text" {
 }
 
 
+variable "customer" {
+  type = string
+  default = "foo"
+}
+
+
+variable "id" {
+  type = number
+  default = "foo"
+}
+
+
 variable "subnet1" {
   type = string
   default = "foo"
