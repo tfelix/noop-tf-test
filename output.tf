409fb8d84f600955c3289e72cb8cbd19f6e01451
Thomas Felix <tfelix@meshcloud.io>
2024-10-01 15:45:15 +0200

    Adds an static object output for testing

--- output.tf
+++ output.tf
@@ -9,3 +9,12 @@ output "text" {
 output "flag" {
   value = var.flag
 }
+
+output "example_output" {
+  value = {
+    id                         = "12345678-1234-1234-1234-1234567890ab"
+    display_name               = "name"
+    owners                     = ["12"]
+    security_enabled           = true
+  }
+}
