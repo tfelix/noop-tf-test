b5c2b064010b6c35a184ac036f41f286d59d62ed
Thomas Felix <tfelix@meshcloud.io>
2024-11-04 16:19:53 +0100

    Adds folder path

---
+++ test/vars.tf
@@ -0,0 +1,47 @@
+variable "num" {
+  type = number
+  default = 0
+}
+
+variable "text" {
+  type = string
+  default = "foo"
+}
+
+
+variable "customer" {
+  type = string
+  default = "foo"
+}
+
+
+variable "id" {
+  type = number
+  default = 1337
+}
+
+
+variable "subnet1" {
+  type = string
+  default = "foo"
+}
+
+variable "subnet2" {
+  type = string
+  default = "foo"
+}
+
+variable "userinput" {
+  type = string
+  default = "foo"
+}
+
+variable "platformTenantId" {
+  type = string
+  default = "foo"
+}
+
+variable "flag" {
+  type = bool
+  default = false
+}
